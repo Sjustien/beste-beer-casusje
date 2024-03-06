91e661edaeb3d5b5987312ad1190053b5b953644
Timo Schuurman <timoschuurman@outlook.com>
2024-03-06 09:57:57 +0100

    JS plane frontend

---
+++ FrontendPlane/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="./style/Navbar.css" />
+    <link rel="stylesheet" href="./style/Footer.css" />
+    <link rel="stylesheet" href="./style/HomePage.css" />
+    <link rel="stylesheet" href="./style/LoginForms.css" />
+  </head>
+  <body>
+    <!-- Include Navbar -->
+    <?php include_once './Pages/Navbar.html'; ?>
+    
+    <!-- Include Home Page Content -->
+    <!-- <?php include_once './Pages/HomePage.html'; ?> -->
+    <?php include_once './Pages/Signup.html'; ?>
+
+    
+    <!-- Include Footer -->
+    <?php include_once './Pages/Footer.html'; ?>
+  </body>
+</html>
